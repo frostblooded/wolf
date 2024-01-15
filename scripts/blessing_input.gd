6a44565cb2dad23192e728970e8c43528ca0eddd
Nikolay Danailov <frostblooded@yahoo.com>
2024-01-15 20:07:21 +0200

    Change UI to make it easier to use on a phone screen

--- scripts/blessing_input.gd
+++ scripts/blessing_input.gd
@@ -2,8 +2,8 @@ extends HBoxContainer
 
 
 func _on_button_pressed():
-    MessageBus.BLESS_BUTTON_PRESSED.emit($BlessAmountSlider.value)
+    MessageBus.BLESS_BUTTON_PRESSED.emit($VBoxContainer/BlessAmountSlider.value)
 
 
 func _on_bless_amount_slider_value_changed(value: float):
-    $Label.text = "Blessing for: %.0f" % value
+    $VBoxContainer/Label.text = "Blessing for: %.0f" % value
