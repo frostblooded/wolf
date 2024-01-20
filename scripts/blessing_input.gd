07593a09a406ec139112769c91a6cdf43a131b2d
Nikolay Danailov <frostblooded@yahoo.com>
2024-01-20 11:12:42 +0200

    Add goal label

--- scripts/blessing_input.gd
+++ scripts/blessing_input.gd
@@ -1,9 +1,11 @@
 extends HBoxContainer
 
 @onready var bless_amount_label: Label = find_child("BlessAmountLabel")
+@onready var bless_goal_label: Label = find_child("BlessGoalLabel")
 
 func _ready():
     update_bless_amount_label(1)
+    bless_goal_label.text = "Goal: %d to 100" % DifficultyManager.get_min_goal()
 
 func _on_button_pressed():
     MessageBus.BLESS_BUTTON_PRESSED.emit($VBoxContainer/BlessAmountSlider.value)
